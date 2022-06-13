d8e86584922108e211b264702d40a4e7c36909c2
Randy Chung <randy.chung@gmail.com>
2022-06-13 16:16:01 -0400

    Cleaned up package.json and added some babel presets.

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-typescript',
+    'module:metro-react-native-babel-preset'
+  ]
+};
